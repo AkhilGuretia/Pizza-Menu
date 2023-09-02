09303cda29ea6b2a0ef15f9b731d693a9c56137e
Akhil Kumar <akhilguretia@gmail.com>
2023-09-02 23:05:45 +0530

    styling sold out pizza component

--- src/index.js
+++ src/index.js
@@ -130,15 +130,14 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) pizzaObj.price = "sold out";
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""} `}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
